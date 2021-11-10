73d11e66b7cd4334c792ae66bedb8de89128c611
alejlatorre <alejandro.latorre@pucp.edu.pe>
2021-11-09 22:46:50 -0500

    added files

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# %%
+# %% 0. Libraries
 import numpy as np
 import pandas as pd
 import geopandas as gpd
+from shapely import wkt
 
-# %% 
+# %% 1. General settings
 option_settings = {
     'display.max_rows': None,
     'display.max_columns': False,
@@ -16,11 +17,17 @@ gpd.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
 IN_PATH = 'data/in/'
 OUT_PATH = 'data/out/'
 
-# %%
+# %% 2. Load data
 filename = 'Arequipa.kml'
-df = gpd.read_file(IN_PATH + filename, driver='KML')
+gdf_aqp = gpd.read_file(IN_PATH + filename, driver='KML')
+gdf_aqp.drop(columns='Description', inplace=True)
+gdf_aqp.columns = map(str.lower, df.columns)
 
-df.drop(columns='Description', inplace=True)
-df.columns = map(str.lower, df.columns)
+filename = 'partner_location.csv'
+df_prt = pd.read_csv(IN_PATH + filename)
+gdf_prt = geopandas.GeoDataFrame(
+    df_prt, 
+    geometry=geopandas.points_from_xy(df_prt.longitude, df_prt.latitude)
+)
 
-# %%
+# %% 
